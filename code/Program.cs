c1738075db28408ade1dec7f8153497e7a20dbe8
Swellshinider <eduardoleal.contact@gmail.com>
2022-07-21 11:36:26 -0300

    clean-program-run

--- code/Program.cs
+++ code/Program.cs
@@ -1,17 +1,14 @@
+using LealPassword.View;
+
 namespace LealPassword
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        internal static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new LoadingView());
         }
     }
 }
