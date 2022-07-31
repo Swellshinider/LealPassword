3632462ae6a3851dcb1ec16b3a53dc3131606a23
Swellshinider <eduardoleal.contact@gmail.com>
2022-07-31 18:43:48 -0300

    project re-generated

---
+++ code/LealPassword/View/MainView.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace LealPassword.View
+{
+    internal sealed partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Name = "MainView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LealPassword";
+            this.Resize += new System.EventHandler(this.MainView_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ code/LealPassword/View/MainView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LealPassword.View
+{
+    internal sealed partial class MainView : Form
+    {
+        private Region GetRegion() => Program.GenerateRoundRegion(Width, Height, 20);
+
+        internal MainView()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            InitializeComponent();
+            Region = GetRegion();
+        }
+
+        private void MainView_Resize(object sender, EventArgs e) => Region = GetRegion();
+
+        #region Override
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x84)
+            {
+                var pos = new Point(m.LParam.ToInt32());
+                pos = PointToClient(pos);
+
+                if (pos.Y < 32)
+                    m.Result = (IntPtr)2;
+
+                if (pos.X >= ClientSize.Width - 16 && pos.Y >= ClientSize.Height - 16)
+                    m.Result = (IntPtr)17;
+            }
+        }
+        #endregion
+    }
+}
