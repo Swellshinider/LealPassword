2f8f6093aa20babef0ae8e41f15d7a41e213fb36
Swellshinider <eduardoleal.contact@gmail.com>
2022-08-08 16:22:45 -0300

    default-config

---
+++ code/LealPassword.Startup/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LealPassword")]
+[assembly: AssemblyDescription("Um gerenciador de senhas completo para facilitar sua vida.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Swellshinider's")]
+[assembly: AssemblyProduct("LealPassword")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ffc86243-904d-487a-a609-f48d487cc77e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
