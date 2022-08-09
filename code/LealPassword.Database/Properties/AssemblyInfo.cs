6f41656ddf9bd5170fb3b71bbc15f83f4f92d082
Swellshinider <eduardoleal.contact@gmail.com>
2022-08-09 12:25:21 -0300

    starting-database-structure

---
+++ code/LealPassword.Database/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LealPassword.Themes")]
+[assembly: AssemblyDescription("Biblioteca de controle de dados do LealPassword.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Swellshinider's")]
+[assembly: AssemblyProduct("LealPassword.Themes")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("57562059-319c-473f-8a25-1f5634659ca0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
