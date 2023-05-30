4becf9297f7a3c15aca378d59b639ae45acc3f98
Swellshinider <eduardoleal.contact@gmail.com>
2023-05-30 11:21:45 -0300

    Edit and viewing cards base

---
+++ code/LealPassword/UI/EditComponentSub/ViewCardUI.cs
@@ -0,0 +1,31 @@
+﻿using LealPassword.Database.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LealPassword.UI.EditComponentSub
+{
+    internal sealed partial class ViewCardUI : UserControl
+    {
+        private readonly Card _card;
+
+        internal ViewCardUI(Card card, Control parent)
+        {
+            _card = card;
+            Dock = DockStyle.Fill;
+            BackColor = parent.BackColor;
+            parent.Controls.Add(this);
+        }
+
+        internal void GenerateObjects()
+        {
+
+        }
+    }
+}
